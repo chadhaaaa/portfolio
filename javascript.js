b25044496a3bf5ffd71d8a8fde1b4663be0ff348
chadhaaaa <61332948+chadhaaaa@users.noreply.github.com>
2020-02-21 16:44:31 -0500

    Portfolio
    Ashutosh chadha website

---
+++ javascript.js
@@ -0,0 +1,13 @@
+function change(){
+	window.alert('Please click on every link to know more about the applicant');
+	document.getElementById("click").innerHTML="Clicked";
+}
+function changeImageDavis(){
+document.getElementById("image").src="davis.jpg";
+}
+function changeImageTrafalgar(){
+document.getElementById("image").src="trafalgar.jpg";
+}
+function changeImageHazel(){
+document.getElementById("image").src="hazel.jpg";
+}
